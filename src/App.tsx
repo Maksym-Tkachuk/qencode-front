ac84dfc80fa1e813be4cfcef7985e26a0337c1e6
Maksym Tkachuk <tkachykmakc@gmail.com>
2024-03-06 18:52:21 +0200

    Make a password input

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import github from 'src/assets/images/svg/github.svg'
 import Button from 'src/components/Button'
 
 import Input from './components/Input'
+import InputPassword from './features/InputPassword'
 import './styles/fonts.css'
 import './styles/normalize.css'
 
 const App = (): JSX.Element => {
   return (
     <>
+      <InputPassword />
       <Input placeholder="Enter email" />
       <Button icon={<img src={github} alt="github" />} variant="outlined">
         Github
---
+++ src/features/InputPassword/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+import eye from 'src/assets/images/svg/eye.svg'
+import Input from 'src/components/Input'
+import s from 'src/features/InputPassword/styles.module.css'
+
+type InputPasswordProps = {
+  onChange?: (value: string) => void
+}
+
+const InputPassword = ({ onChange }: InputPasswordProps): JSX.Element => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const handleVisible = (): void => {
+    setIsVisible(prev => !prev)
+  }
+
+  return (
+    <div className={s.container}>
+      <Input type={isVisible ? 'text' : 'password'} onChange={onChange} />
+      <div
+        onClick={handleVisible}
+        onKeyDown={handleVisible}
+        role="button"
+        tabIndex={0}
+        className={s.eye_icon}
+      >
+        <img src={eye} alt="eye" />
+      </div>
+    </div>
+  )
+}
+
+export default InputPassword
